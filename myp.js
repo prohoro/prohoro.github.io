ec3e0ff34be9218c7a36225bce37bbea808ffec6
prohoro <iprohorov2319@gmail.com>
2017-12-11 00:03:32 +0300

    Add files via upload

--- myp.js
+++ myp.js
@@ -1,14 +1,30 @@
-var buttonStart, song, an;
+var buttonStart, song, an, buttonSelect;
+var count = 0;
 function setup() {
-  createCanvas(window.innerWidth , 480);
+  createCanvas(window.innerWidth , 500);
   buttonStart = createButton('clean');
+  buttonSelect = createButton('select');
+  buttonSelect.mousePressed(Select);
   buttonStart.mousePressed(start);  
 }
 function draw() {
-    ellipse(mouseX, mouseY, 80, 80);
-    
+        if (mouseIsPressed){
+          if (count == 0)
+            rect( mouseX, mouseY, mouseX+80, mouseX-80 );
+          if (count == 1)
+            ellipse(mouseX, mouseY, 80, 80);
+          if (count == 2)
+            rect( mouseX, mouseY, mouseX-80, mouseX+80 );
+        }
 }
 function start(){
   console.log();
   rect( 0, 0, width, height );
+}
+function Select (){
+  if (count > 1)
+    count = 0;
+  else
+    count++;
+  console.log(count);
 }
